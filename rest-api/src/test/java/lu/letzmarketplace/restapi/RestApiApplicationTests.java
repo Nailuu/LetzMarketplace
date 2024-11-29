9c2f1dc6c47bd7aa5d6c24483e99b4030c057678
Nailu <contact@nailu.fr>
2024-11-29 16:19:04 +0100

    Initial commit, setup rest api backend skeleton

---
+++ rest-api/src/test/java/lu/letzmarketplace/restapi/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lu.letzmarketplace.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
