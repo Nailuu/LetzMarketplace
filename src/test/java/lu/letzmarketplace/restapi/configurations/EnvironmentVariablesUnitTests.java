42b3acb3ea8ed83bcb758af0369a1329150a5681
Nailuu <contact@nailu.fr>
2024-12-08 00:21:43 +0100

    Update unit test for env variables, update pipeline to upload new release on github repo

--- src/test/java/lu/letzmarketplace/restapi/configurations/EnvironmentVariablesUnitTests.java
+++ src/test/java/lu/letzmarketplace/restapi/configurations/EnvironmentVariablesUnitTests.java
@@ -4,16 +4,21 @@ import io.github.cdimascio.dotenv.Dotenv;
 import io.jsonwebtoken.io.Decoders;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
 public class EnvironmentVariablesUnitTests {
-    Dotenv dotenv = Dotenv.configure().load();
+    @Autowired
+    private Environment env;
 
     @Test
     @DisplayName("CORS")
     public void testCORS() {
-        String var = dotenv.get("CORS");
+        String var = env.getProperty("CORS");
 
         assertThat(var).isNotNull();
         assertThat(var).isNotEmpty();
@@ -22,7 +27,7 @@ public class EnvironmentVariablesUnitTests {
     @Test
     @DisplayName("JWT_SECRET_KEY")
     public void testJwtSecretKey() {
-        String var = dotenv.get("JWT_SECRET_KEY");
+        String var = env.getProperty("JWT_SECRET_KEY");
 
         assertThat(var).isNotNull();
         assertThat(var).isNotEmpty();
@@ -32,7 +37,7 @@ public class EnvironmentVariablesUnitTests {
     @Test
     @DisplayName("DB_HOST")
     public void testDbHost() {
-        String var = dotenv.get("DB_HOST");
+        String var = env.getProperty("DB_HOST");
 
         assertThat(var).isNotNull();
         assertThat(var).isNotEmpty();
@@ -41,7 +46,7 @@ public class EnvironmentVariablesUnitTests {
     @Test
     @DisplayName("DB_PORT")
     public void testDbPort() {
-        String var = dotenv.get("DB_PORT");
+        String var = env.getProperty("DB_PORT");
 
         assertThat(var).isNotNull();
         assertThat(var).isNotEmpty();
@@ -50,7 +55,7 @@ public class EnvironmentVariablesUnitTests {
     @Test
     @DisplayName("DB_NAME")
     public void testDbName() {
-        String var = dotenv.get("DB_NAME");
+        String var = env.getProperty("DB_NAME");
 
         assertThat(var).isNotNull();
         assertThat(var).isNotEmpty();
@@ -59,7 +64,7 @@ public class EnvironmentVariablesUnitTests {
     @Test
     @DisplayName("DB_USERNAME")
     public void testDbUsername() {
-        String var = dotenv.get("DB_USERNAME");
+        String var = env.getProperty("DB_USERNAME");
 
         assertThat(var).isNotNull();
         assertThat(var).isNotEmpty();
@@ -68,7 +73,7 @@ public class EnvironmentVariablesUnitTests {
     @Test
     @DisplayName("DB_PASSWORD")
     public void testDbPassword() {
-        String var = dotenv.get("DB_PASSWORD");
+        String var = env.getProperty("DB_PASSWORD");
 
         assertThat(var).isNotNull();
         assertThat(var).isNotEmpty();
@@ -77,7 +82,7 @@ public class EnvironmentVariablesUnitTests {
     @Test
     @DisplayName("TEST_DB_HOST")
     public void testTestDbHost() {
-        String var = dotenv.get("TEST_DB_HOST");
+        String var = env.getProperty("TEST_DB_HOST");
 
         assertThat(var).isNotNull();
         assertThat(var).isNotEmpty();
@@ -86,7 +91,7 @@ public class EnvironmentVariablesUnitTests {
     @Test
     @DisplayName("TEST_DB_PORT")
     public void testTestDbPort() {
-        String var = dotenv.get("TEST_DB_PORT");
+        String var = env.getProperty("TEST_DB_PORT");
 
         assertThat(var).isNotNull();
         assertThat(var).isNotEmpty();
@@ -95,7 +100,7 @@ public class EnvironmentVariablesUnitTests {
     @Test
     @DisplayName("TEST_DB_NAME")
     public void testTestDbName() {
-        String var = dotenv.get("TEST_DB_NAME");
+        String var = env.getProperty("TEST_DB_NAME");
 
         assertThat(var).isNotNull();
         assertThat(var).isNotEmpty();
@@ -104,7 +109,7 @@ public class EnvironmentVariablesUnitTests {
     @Test
     @DisplayName("TEST_DB_USERNAME")
     public void testTestDbUsername() {
-        String var = dotenv.get("TEST_DB_USERNAME");
+        String var = env.getProperty("TEST_DB_USERNAME");
 
         assertThat(var).isNotNull();
         assertThat(var).isNotEmpty();
@@ -113,7 +118,7 @@ public class EnvironmentVariablesUnitTests {
     @Test
     @DisplayName("TEST_DB_PASSWORD")
     public void testTestDbPassword() {
-        String var = dotenv.get("TEST_DB_PASSWORD");
+        String var = env.getProperty("TEST_DB_PASSWORD");
 
         assertThat(var).isNotNull();
         assertThat(var).isNotEmpty();
